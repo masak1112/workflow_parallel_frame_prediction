a08487647d6e14316e358c5ed1ea999316622393
Bing Gong <gongbing1112@gmail.com>
2019-11-05 15:57:58 +0100

    remove packageInstallation.sh

--- DataPreprocess/packageInstallation.sh
+++
@@ -1,9 +0,0 @@
-module --force purge
-module /usr/local/software/jureca/OtherStages
-module load Stages/2019a
-module load Intel/2019.3.199-GCC-8.3.0  ParaStationMPI/5.2.2-1-mt
-module load mpi4py/3.0.1-Python-3.6.8
-pip3 install --user netCDF4
-pip3 install --user numpy
-
-
